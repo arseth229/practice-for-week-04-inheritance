19de4f4a6f4ce9430a16b779f524b6a7575eacd5
arseth229 <146048786+arseth229@users.noreply.github.com>
2024-01-19 16:12:01 -0600

    finished

--- classes/student.js
+++ classes/student.js
@@ -1,6 +1,26 @@
 const Person = require('./person');
 
-// Your code here
+class Student extends Person {
+  constructor (firstName, lastName, major, GPA) {
+  super (firstName, lastName);
+  this.major = major;
+  this.GPA = GPA;
+  }
+
+  static compareGPA (stu1, stu2) {
+    if (stu1.GPA > stu2.GPA) {
+      return (`${stu1.firstName} ${stu1.lastName} has the higher GPA.`);
+    }
+    if (stu1.GPA < stu2.GPA) {
+      return (`${stu2.firstName} ${stu2.lastName} has the higher GPA.`);
+    }
+    if (stu1.GPA == stu2.GPA) {
+       return (`Both students have the same GPA`);
+    }
+  }
+}
+
+
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
--- classes/teacher.js
+++ classes/teacher.js
@@ -1,6 +1,20 @@
 const Person = require('./person');
 
-// Your code here
+class Teacher extends Person {
+  constructor (firstName, lastName, subject, yearsOfExperience) {
+    super(firstName, lastName);
+    this.subject = subject;
+    this.yearsOfExperience = yearsOfExperience;
+  }
+
+  static combinedYearsOfExperience(teacher) {
+    let combined = 0;
+    for (const exp of teacher) {
+      combined = combined + exp.yearsOfExperience;
+    }
+    return combined;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
